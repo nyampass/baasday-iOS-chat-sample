47188023dda8f4497af47439ce3a15c98b2b7028
Yuu Shimizu <yuu@nyampass.com>
2013-05-28 05:13:08 +0900

    Initial Commit

---
+++ BaasdaySampleChat/BSCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSCAppDelegate.h
+//  BaasdaySampleChat
+//
+//  Created by Yuu Shimizu on 5/28/13.
+//  Copyright (c) 2013 Nyampass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BaasdaySampleChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BaasdaySampleChat
+//
+//  Created by Yuu Shimizu on 5/28/13.
+//  Copyright (c) 2013 Nyampass. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BSCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([BSCAppDelegate class]));
+	}
+}
